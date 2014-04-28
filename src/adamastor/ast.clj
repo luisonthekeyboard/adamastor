efc11bfce06d5eac92f7a9f20231b90092c9cdfc
Luis Rodrigues Soares <luizsoarez@outlook.com>
2014-04-28 11:57:13 +0200

    Added documentation to `paragraph`.

--- src/adamastor/ast.clj
+++ src/adamastor/ast.clj
@@ -14,6 +14,12 @@
     [(trimr string)]))
 
 (defn ^:dynamic paragraph [lines]
+  "A paragraph is simply one or more consecutive lines of text, separated by
+  one or more blank lines. (A blank line is any line that looks like a
+  blank line — a line containing nothing but spaces or tabs is considered
+  blank.) Normal paragraphs should not be indented with spaces or tabs.
+  When you do want to insert a <br /> break tag using Markdown, you end a
+  line with two or more spaces, then type return."
   (loop [lines lines
          paragraph-text []]
     (if (empty? lines)
