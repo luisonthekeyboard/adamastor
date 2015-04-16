0e88baf1d2642d536f6636adf91fbe714e073efd
Luis <luis@decomputed.com>
2015-04-16 08:49:47 +0200

    Updates JDK version for travis and project repo URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject adamastor "0.2.0"
   :description "Destroying caravels since the 1400s..."
-  :url "https://github.com/luizsoarez/adamastor"
+  :url "https://github.com/decomputed/adamastor"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
